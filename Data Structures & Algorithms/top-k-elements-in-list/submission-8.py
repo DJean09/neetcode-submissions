f380b07565638bb39c35728b1708bd2c625fdc3a
Dominick <78790285+DJean09@users.noreply.github.com>
2026-06-16 15:51:47 -0400

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Counts how many times each number appears
+        count = {}
+        for num in nums:
+            # Add to the existing value in num
+            # hashMap.get(key, default fallback value if empty)
+            count[num] = 1 + count.get(num, 0)
+        
+        # building the heap. import heapq for this.
+        heap = []
+        for num in count.keys():
+            # using heapq to push the values in while sorting them based on frequency.
+            # heapq.heappush(the list being pushed into, (frequency, the actual number))
+            heapq.heappush(heap, (count[num], num))
+            # Once k has been reached. We pop out the element with the lowest frequency
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        # pop all the elements from the heap and collect their numbers into the result list
+        # heapq.heapop(list we're taking from)[taking from the second item only]
+        result = []
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+        
+        return result
