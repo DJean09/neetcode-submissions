2329cb24fdc4e80864f11f4360c4856ae92849d0
Dominick <78790285+DJean09@users.noreply.github.com>
2026-06-18 03:09:19 -0400

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,44 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # initialize an empty result builder
+        res = []
+
+        # for each string in the list
+        # compute its length
+        # append "length#string" to the builder
+        for s in strs:
+            res.append(str(len(s)))
+            res.append("#")
+            res.append(s)
+        
+        # return the entire list as a joined string
+        return "".join(res)
+
+    def decode(self, s: str) -> List[str]:
+        # initialize an empty list for the decoded strings and a pointer i = 0
+        res = []
+        i = 0
+        j = 0
+
+        # while i is within the bounds of the encoded string
+        while i < len(s):
+            # move j forward until it finds # for the length
+            while s[j] != '#':
+                j += 1
+            
+            #convert the substring s[i:j] into an integer length
+            length = int(s[i:j])
+
+            # move i to the next character after #
+            i = j + 1
+            # extract the next length characters
+            j = i + length
+            # append the extracted string to the list
+            res.append(s[i:j])
+
+            # move i forward by length to continue
+            i = j
+        
+        # return the decoded list
+        return res
